4842a0b5620a03cd1ffa8008e750db6412a2e1aa
odedbartov <odedoded777@gmail.com>
2019-01-03 16:40:35 +0200

    Make an api for token validation

---
+++ Authentication/Authentication.Server/Controllers/ValidateTokenController.cs
@@ -0,0 +1,30 @@
+﻿using Authentication.Common.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Authentication.Server.Controllers
+{
+    public class ValidateTokenController : ApiController
+    {
+        IValidateToken repository;
+        const string api = "api/ValidateToken/";
+
+        public ValidateTokenController(IValidateToken manager)
+        {
+            repository = manager;
+        }
+
+
+        [HttpGet]
+        [Route(api + "Validate/{token}")]
+        public string Validate(string token)
+        {
+            string result = repository.ValidateToken(token);
+            return result;
+        }
+    }
+}
