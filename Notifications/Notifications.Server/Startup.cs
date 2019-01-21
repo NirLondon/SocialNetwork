1a26ea94c1aa3ac9abdfa40a03210f680417dbd8
NirLondon <39864310+NirLondon@users.noreply.github.com>
2019-01-21 16:57:42 +0200

    init

---
+++ Notifications/Notifications.Server/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+using System.Configuration;
+
+namespace Notifications.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
